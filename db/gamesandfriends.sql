336923aff227cfaecc1d85c058d97bca4e2db4d3
José Millán Fernández <jose.millan@iesdonana.org>
2019-10-28 20:22:59 +0100

    Añado datos a la BBDD

--- db/gamesandfriends.sql
+++ db/gamesandfriends.sql
@@ -37,6 +37,7 @@ CREATE TABLE productos
   , nombre        VARCHAR(255)      NOT NULL UNIQUE
   , descripcion   TEXT              NOT NULL
   , precio        NUMERIC(6,2)
+  , stock         NUMERIC(5)        NOT NULL
   , juego_id      BIGINT            REFERENCES juegos(id)
                                     ON DELETE CASCADE
                                     ON UPDATE CASCADE
@@ -51,6 +52,7 @@ CREATE TABLE criticas
   , created_at    TIMESTAMP(0)      NOT NULL
                                     DEFAULT CURRENT_TIMESTAMP
   , valoracion    NUMERIC(1)        NOT NULL
+                                    CHECK (valoracion > 0)
   , usuario_id    BIGINT            NOT NULL
                                     REFERENCES usuarios(id)
                                     ON DELETE NO ACTION
@@ -143,3 +145,29 @@ CREATE TABLE juegos_etiquetas
 INSERT INTO usuarios (nombre, password, email)
 VALUES ('admin', crypt('hnmpl', gen_salt('bf', 10)), 'admin@aculturese.com'),
 ('pepe', crypt('pepe', gen_salt('bf', 10)), 'jose.millan@iesdonana.org');
+
+INSERT INTO juegos (titulo, descripcion, fechaLan, dev)
+VALUES ('Rocket League', 'Futbol con coches teledirigidos equipados con un cohete. Una entrega de juego basado en fisicas con el motor Unreal Engine.', '2015-07-07', 'Psyonix Inc.'),
+('The Binding of Isaac: Rebirth', 'Adéntrate en el sótano intentando huir de tu asesina, un juego Rogue-Like con esteticas bizarras y miles de secretos.', '2014-11-04', 'Nicalis Inc.');
+
+INSERT INTO productos (nombre, descripcion, precio, stock, juego_id)
+VALUES ('Rocket League', 'Futbol con coches teledirigidos equipados con un cohete. Una entrega de juego basado en fisicas con el motor Unreal Engine.', 19.99, 9001, 1),
+('The Binding of Isaac: Rebirth', 'Adéntrate en el sótano intentando huir de tu asesina, un juego Rogue-Like con esteticas bizarras y miles de secretos.', 14.99, 9001, 2),
+('Funko POP de psyco de Borderlands 3', 'De los juegos de Borderlands, llega el Funko POP de Psyco, los maniaticos al frente de los grupos hostiles en Pandora.', 19.99, 5, null);
+
+INSERT INTO criticas (opinion, created_at, valoracion, usuario_id, producto_id)
+VALUES ('Pues es un juegazo, me encanta', null, 9, 2, 1),
+('Es algo turbio, pero me gusta, los enemigos son muy raros, tiene su encanto', null, 9, 2, 2),
+('Pues a mi los Funkos no me gustan, pero tener un psyco en mi cuarto me mola', null, 5, 2, 3);
+
+INSERT INTO posts (titulo, created_at, desarrollo, juego_id, usuario_id)
+VALUES ('Primer post', null, 'Cuando empece el proyecto hice este post, para crear una prueba y aqui se quedó la prueba por ahora, ya la cambiare, pero por ahora, asi se mantendrá.', 2, 2);
+
+INSERT INTO comentarios (created_at, texto, usuario_id, post_id)
+VALUES (null, 'Pues me gusta tu post, no lo cambies', 1, 1),
+(null, 'jejeje, me he comentado a mi mismo', 2, 1);
+
+INSERT INTO etiquetas (nombre)
+VALUES ('Deportes'), ('Carreras'), ('Competitivo'), ('Casual'), ('Adictivo'),
+('Rogue-like'), ('Multijugador'), ('Indie'), ('Dificil'), ('Un jugador'),
+('RPG'), ('Shooter'), ('FPS'), ('Accion');
