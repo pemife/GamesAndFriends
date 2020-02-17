b4bfa7b33890a34dfab8489ef33bc825e508dddf
pepe millan fernandez <jose.millan@iesdonana.org>
2020-02-17 18:57:34 +0100

    Trabajando en script

--- scripts/juegosCreate.sh
+++ scripts/juegosCreate.sh
@@ -36,12 +36,11 @@ case $url in
     steampowered )
         nombreJuego=`egrep "apphub_AppName" .paginaJuego.html | cut -d">" -f2 | cut -d"<" -f1`
 
-        descripcionJuego=`awk '/<div class="game_description_snippet">/,/<\/div>/' .paginaJuego.html | cut -d"<" -f1`
+        descripcionJuego=`awk '/<div class="game_description_snippet">/,/<\/div>/' .paginaJuego.html | cut -d"<" -f1 | sed -n '1!p'`
 
         fechaLan=`awk '/<div class="date">/,/<\/div>/' .paginaJuego.html | cut -d">" -f2 | cut -d"<" -f1`
         fechaDia=`echo ${fechaLan:0:2}`
         mesSinFormato=`echo $fechaLan | cut -d" " -f2 | cut -d"," -f1`
-        echo $mesSinFormato
         fechaAnio=`echo $fechaLan | cut -d" " -f3`
         case $mesSinFormato in
           Ene|Jan ) fechaMes=01;;
@@ -57,26 +56,51 @@ case $url in
           Nov ) fechaMes=11;;
           Dic|Dec ) fechaMes=12;;
         esac
-        fechaFormateada="$fechaAnio-$fechaMes-$fechaDia"
+        fechaFormateada="${fechaAnio}-${fechaMes}-${fechaDia}"
 
         dev=`awk '/<div class="summary column" id="developers_list">/,/<.div>/' .paginaJuego.html | cut -d">" -f2 | cut -d"<" -f1`
         # dev=`awk '/<div class="summary column" id="developers_list">/,/<\/div>/' .paginaJuego.html | awk '/<a.*>/,/<.a>/'`
 
         pub=`grep -m1 'publisher' .paginaJuego.html | cut -d">" -f2 | cut -d"<" -f1`
 
-        echo "Titulo: " $nombreJuego
-        echo "Descripcion: " $descripcionJuego
-        echo "FechaLanzamiento: " $fechaFormateada
-        echo "Desarrolladora: " $dev
-        echo "Editora: " $pub
+        echo "Titulo: " ${nombreJuego}
+        echo "Descripcion: " ${descripcionJuego}
+        echo "FechaLanzamiento: " ${fechaFormateada}
+        echo "Desarrolladora: " ${dev}
+        echo "Editora: " ${pub}
 
-        sudo -u postgres psql -d gamesandfriends -c "INSERT INTO juegos (titulo, descripcion, fechaLan, dev, publ) VALUES ('${nombreJuego}', '${descripcionJuego}', '${fechaFormateada}', '${dev}', '${publ}');"
+        sudo -u postgres psql -d gamesandfriends -c "INSERT INTO juegos (titulo, descripcion, fechaLan, dev, publ) VALUES ('${nombreJuego}', '${descripcionJuego}', '${fechaFormateada}', '${dev}', '${pub}');"
         # 2>/dev/null
     ;;
-    nintendo ) echo "es un enlace de nintendo eshop";;
+    nintendo )
+        nombreJuego=`awk '/<title>/' .paginaJuego.html | cut -d">" -f2 | cut -d"|" -f1`
+        nombreFormatoUrl=`echo $nombreJuego | tr " " +`
+
+        {
+            wget -O .busquedaBing.html https://bing.com/search?q=$nombreFormatoUrl
+        } &> /dev/null
+        descripcion=`grep -oP '/<p class="b_paractl">.*<.p>/' .busquedaBing.html`
+        # Trabajando en la descripcion del juego por parte de la busqueda de bing
+
+        fechaLan=`awk '/Fecha de lanzamiento:/' .paginaJuego.html | cut -d":" -f2 | cut -d"<" -f1`
+        fechaFormateada=${fechaLan:7:11}${fechaLan:3:4}${fechaLan:1:2}
+
+        dev=`awk '/class="game_info_title">Desarrollador<.p>/,/"game_info_text">.*<.p>/' .paginaJuego.html | awk '/text">/,/<.p>/' | cut -d">" -f2 | cut -d"<" -f1`
+        if [[ $dev == "" ]]; then
+            dev="Nintendo"
+        fi
+
+        pub=`awk '/class="game_info_title">Distribuidor<.p>/,/"game_info_text">.*<.p>/' .paginaJuego.html | awk '/text">/,/<.p>/' | cut -d">" -f2 | cut -d"<" -f1`
+
+        echo $nombreJuego
+        echo $descripcion
+        echo $fechaFormateada
+        echo $dev
+        echo $pub
+    ;;
     playstation ) echo "es un enlace de la tienda de playstation";;
     microsoft ) echo "es un enlace de la tienda de xbox";;
     * ) echo "de donde $\#@%! es esto?";;
 esac
 
-rm .paginaJuego.html .urlJuego.html
+rm .paginaJuego.html .urlJuego.html .busquedaBing.html
