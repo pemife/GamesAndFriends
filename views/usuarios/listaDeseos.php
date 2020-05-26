e1226b9ff602ca5efc742bc1be92e5b62e74b8b0
José Millán Fernández <jose.millan@iesdonana.org>
2020-05-26 18:59:52 +0200

    Ordenacion de lista de deseos con JQueryUI sortable

--- views/usuarios/listaDeseos.php
+++ views/usuarios/listaDeseos.php
@@ -17,57 +17,24 @@ $(function(){
     $('#tablasOrden').hide();
     $('#botonGuardar').hide();
 
-    var nuevoOrden = [];
-    nuevoOrden[0] = null;
-    var contador = 1;
-    var botonPrimeraVez = true;
-    
-    $('#botonOrdenar').click(function (e){
-        e.preventDefault();
-        $('#listaJuegos').toggle();
-        $('#tablasOrden').toggle();
-        $('#botonGuardar').toggle();
-        if (botonPrimeraVez) {
-            alert('Ordena los juegos añadiendo segun tu preferencia');
-            botonPrimeraVez = false;
-        }
-    });
-
-    $('.botonOrden').click(function (e){
-        e.preventDefault();
-        // console.log(e.currentTarget.parentElement.parentElement);
-        var jId = e.currentTarget.parentElement.parentElement.dataset.juegoid;
-        var jOr = e.currentTarget.parentElement.parentElement.dataset.juegoorden;
-        var jNo = e.currentTarget.parentElement.parentElement.dataset.juegonombre;
-
-        nuevoOrden[contador] = Number(jId);
-        contador++;
-
-        console.log(nuevoOrden);
-        var nuevaFila = '<tr><td>' + jOr + '</td><td>' + jNo + '</td></tr>';
-
-        $("#tablaDespues").append(nuevaFila);
-        e.currentTarget.parentElement.parentElement.style.display = "none";
-    });
-
     $('#botonGuardar').click(function (e){
         e.preventDefault();
-        if (nuevoOrden.length != $totalJuegos+1) {
+        if (nuevoOrden.length != $totalJuegos) {
             alert('Primero ordena la lista completa!');
         } else {
             //AJAX con nuevo orden
-            nuevoOrdenJson = JSON.stringify(nuevoOrden);
             $.ajax({
                 method: 'POST',
                 url: '$url',
                 data: {uId: $uId, nO: nuevoOrden},
                 dataType: 'json',
                 success: function(result){
-                  if (result) {
-                    window.location = '$url2';
-                  } else {
-                    alert('No ha funcionado, intentalo de nuevo mas tarde');
-                  }
+                    console.log(result);
+                    if (result) {
+                        window.location = '$url2';
+                    } else {
+                        alert('No ha funcionado, intentalo de nuevo mas tarde');
+                    }
                 }
               });
         }
@@ -77,26 +44,21 @@ $(function(){
         update: function(e, ui){
             $('#botonGuardar').show();
             nuevoOrden = devolverNuevoOrden();
-
         }
     });
 
+    $('#listaJuegos').disableSelection();
+
     function devolverNuevoOrden(){
-        var juegos = document.getElementsByClassName('juego');
-        console.log(juegos);
         var ordenJuegos = new Array();
-        juegos.forEach(function () {
-            console.log(this);
-        });
+        var juegos = $('.juego');
+
+        for (var i = 0; i < juegos.length; i++) {
+            ordenJuegos.push(juegos[i].dataset.juegoid);
+        }
 
         return ordenJuegos;
     }
-
-    $('#listaJuegos').disableSelection();
-
-    // var appendTo = $( ".juego" ).sortable( "option", "appendTo" );
- 
-    // $( ".juego" ).sortable( "option", "appendTo", $('.listaJuegos') );
 }); 
 SCRIPT;
 $this->registerJS($js);
@@ -107,41 +69,7 @@ $this->registerJS($js);
 
 <p>Puedes ordenar la lista arrastrando los juegos</p>
 
-<?= Html::a('Ordenar Lista', '#', ['class' => 'btn btn-info mb-2', 'id' => 'botonOrdenar', 'hidden' => (Yii::$app->user->id != $uId)]) ?>
 <?= Html::a('Guardar Orden', '#', ['class' => 'btn btn-success ml-2 mb-2', 'id' => 'botonGuardar']) ?>
-
-<div id="tablasOrden" class="row mt-2 mb-2">
-    <div class="col-md-4">
-        <h3>Orden antes</h3>
-        <table border="1" align="left" class="table">
-            <tr>
-                <th>Orden</th>
-                <th>Juego</th>
-            </tr>
-            <?php foreach ($deseadosProvider->getModels() as $model) { ?>
-                <tr
-                name="juegoDeseado"
-                data-juegoId="<?= $model->juego->id ?>"
-                data-juegoOrden="<?= $model->orden?>"
-                data-juegoNombre="<?= $model->juego->titulo ?>"
-                >
-                    <td name="ordenJuego"><?= $model->orden ?></td>
-                    <td name="nombreJuego"><?= $model->juego->titulo ?></td>
-                    <td><button class="botonOrden"><span class="glyphicon glyphicon-arrow-right"></span></button></td>
-                </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <div class="col-md-4">
-        <h3>Orden Despues</h3>
-        <table border="1" id="tablaDespues" class="table">
-            <tr>
-                <th>Orden</th>
-                <th>Juego</th>
-            </tr>
-        </table>
-    </div>
-</div>
 
 <div id="listaJuegos">
 
@@ -151,7 +79,12 @@ $this->registerJS($js);
         'itemView' => function ($model, $key, $index, $widget) {
             $urlImagen = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.icon-icons.com%2Ficons2%2F510%2FPNG%2F512%2Fgame-controller-b_icon-icons.com_50382.png&f=1&nofb=1¡';
             ?>
-            <div class="row juego" border="1">
+            <div 
+            class="row juego"
+            data-juegoId="<?= $model->juego->id ?>"
+            data-juegoOrden="<?= $model->orden?>"
+            data-juegoNombre="<?= $model->juego->titulo ?>"
+            >
                 <div class="row col-md-12">
                     <h3><?= Html::encode($model->juego->titulo) ?></h3>
                 </div>
@@ -197,7 +130,7 @@ $this->registerJS($js);
                     }
                     ?>
                 </div>
-                <hr>
+                <div class="col-md-12"><hr></div>
             </div>
             <?php
         },
